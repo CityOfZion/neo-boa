5f564d1cba662ade5c7a1b0191984001c620d1db
Thomas Saunders <tasaunders@gmail.com>
2017-10-14 10:17:04 -0500

    added support for app calls

---
+++ boa/tests/src/blockchain/AppCallTest.py
@@ -0,0 +1,19 @@
+from boa.blockchain.vm.Neo.App import RegisterAppCall
+from boa.blockchain.vm.Neo.Runtime import Notify
+
+Fibo = RegisterAppCall('24196a584e2bd4c343148e553a7bca9738ae3b19', intval=0)
+
+def Main():
+
+    fibtest = 7
+
+    res = Fibo(fibtest)
+
+    print("did fibo!")
+
+    Notify(res)
+
+    output = res + 18
+
+
+    return output
