d0f77d806cfa3d7a2bd9a58b2e8337a32653ae3c
Thomas Saunders <tasaunders@gmail.com>
2017-10-15 10:50:59 -0500

    Added updates for importing contract addr with n number params.  Added updates to user wallet.  added shift left and right operators.  updated UA string.  updated NeoNode functionality

--- boa/tests/src/ListCompTest.py
+++ boa/tests/src/ListCompTest.py
@@ -1,17 +1,17 @@
-
+from boa.blockchain.vm.Neo.Runtime import Notify
 
 def Main():
 
-    items = [1,3, 4, 5, 8, 12]
+    items = [1,3]
 
-
-    for i in items:
-        print(i)
-
+#    j = 0
+#    for i in items:
+#        j += i
 #    i2 = [i+1 if i > 0 else i + 5 for i in items]
 
-    [print(i) for i in items]
+#    m = 0
+    [Notify(i) for i in items] # this is not working
 
+    m = 3
 
-
-    return 1
+    return m
