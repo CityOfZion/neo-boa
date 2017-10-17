2ca7607c627f5127b961be66c5717f73d95fe169
Thomas Saunders <tasaunders@gmail.com>
2017-10-17 08:11:25 -0500

    adding setup for pip distribution

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='neo-boa',
+      version='0.1',
+      description='Python compiler for Neo Virtual Machine',
+      author='Thomas Saunders',
+      author_email='tasunders@gmail.com',
+      url='',
+      packages=['boa',],
+     )
