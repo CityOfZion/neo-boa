412a9ad3dae6ea1bd019f6067eb21ae8a2bb506b
Taconut <alex@alexhulbert.com>
2018-03-17 11:05:59 -0400

    Added support for InvocationTransaction/GetScript Goes with CityOfZion/neo-python pull #337

---
+++ boa/interop/Neo/InvocationTransaction.py
@@ -0,0 +1,19 @@
+
+class InvocationTransaction:
+
+    @property
+    def GetScript(self):
+        """
+
+        :return:
+        """
+        return GetScript(self)
+
+
+def GetScript(invocation_transaction):
+    """
+
+    :param invocation_transaction:
+    """
+    pass
+
