0b89c194c0b97936985b6180915eaa0db7fdd9c1
Thomas Saunders <tasaunders@gmail.com>
2018-03-16 22:03:47 -0500

    cleanup requirements

--- boa_test/tests/test_dict.py
+++ boa_test/tests/test_dict.py
@@ -3,8 +3,8 @@ from boa.compiler import Compiler
 from neo.Settings import settings
 from neo.Prompt.Commands.BuildNRun import TestBuild
 
-settings.set_log_vm_instruction(True)
-settings.log_smart_contract_events = True
+#settings.set_log_vm_instruction(True)
+#settings.log_smart_contract_events = True
 
 
 class TestContract(BoaTest):
