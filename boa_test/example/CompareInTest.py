66ef74ef0dfcbc6e9ebc310e86d94ea0321cd18b
Thomas Saunders <tasaunders@gmail.com>
2018-04-28 19:28:50 -0500

    add tests for comparison operator IN

---
+++ boa_test/example/CompareInTest.py
@@ -0,0 +1,60 @@
+
+def Main(operation):
+
+    mylist = [1, 4, 10]
+
+    myDict = {
+        'a': 1,
+        'b': 34,
+        'c': 'abc'
+
+    }
+
+    if operation == 1:
+
+        return 'a' in myDict
+
+    elif operation == 2:
+
+        return 3 in myDict
+
+    elif operation == 3:
+
+        # this should return true
+        # the neo-vm uses 'HAS_KEY' op for lists
+        # which only checks the length of the array :(
+        # it doesn't test whether the array
+        # contains the value
+        return 2 in mylist
+
+    elif operation == 4:
+        # this should return false
+        # even though 10 is in the list
+        # because the list isn't 10 items long
+        return 10 in mylist
+
+    elif operation == 5:
+
+        myDict['h'] = 3
+
+        return 'h' in myDict
+
+    elif operation == 6:
+
+        mylist.append(8)
+
+        return 3 in mylist
+
+    elif operation == 7:
+
+        myDict[7] = 'a'
+
+        return 7 in myDict
+
+    elif operation == 8:
+
+        myDict.remove('a')
+
+        return 'a' in myDict
+
+    return False
---
+++ boa_test/tests/test_compare_in.py
@@ -0,0 +1,44 @@
+from boa_test.tests.boa_test import BoaTest
+from boa.compiler import Compiler
+
+from neo.Prompt.Commands.BuildNRun import TestBuild
+
+
+class TestContract(BoaTest):
+
+    def test_compare_in(self):
+
+        output = Compiler.instance().load('%s/boa_test/example/CompareInTest.py' % TestContract.dirname).default
+        out = output.write()
+
+        tx, results, total_ops, engine = TestBuild(out, [1], self.GetWallet1(), '', '01')
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0].GetBoolean(), True)
+
+        tx, results, total_ops, engine = TestBuild(out, [2], self.GetWallet1(), '', '01')
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0].GetBoolean(), False)
+
+        tx, results, total_ops, engine = TestBuild(out, [3], self.GetWallet1(), '', '01')
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0].GetBoolean(), True)
+
+        tx, results, total_ops, engine = TestBuild(out, [4], self.GetWallet1(), '', '01')
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0].GetBoolean(), False)
+
+        tx, results, total_ops, engine = TestBuild(out, [5], self.GetWallet1(), '', '01')
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0].GetBoolean(), True)
+
+        tx, results, total_ops, engine = TestBuild(out, [6], self.GetWallet1(), '', '01')
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0].GetBoolean(), True)
+
+        tx, results, total_ops, engine = TestBuild(out, [7], self.GetWallet1(), '', '01')
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0].GetBoolean(), True)
+
+        tx, results, total_ops, engine = TestBuild(out, [8], self.GetWallet1(), '', '01')
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0].GetBoolean(), False)
