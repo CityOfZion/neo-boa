4c1b4b2d8e58d96fab0cdc739bca3f8e3814c29f
Erik van den Brink <git@erikvandenbrink.com>
2020-02-04 20:00:47 +0100

    add cli, change bytecode dependency to upstream

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
 
 
     install_requires=[
-        'coz-bytecode==0.5.1',
+        'bytecode==0.10.0',
         'astor'
     ],
 
@@ -104,9 +104,9 @@ setup(
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    #entry_points={
-    #    'console_scripts': [
-    #        'sample=sample:main',
-    #    ],
-    #},
+    entry_points={
+       'console_scripts': [
+           'neo-boa=boa.cli:main',
+       ],
+    },
 )
